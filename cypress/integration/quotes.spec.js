2cb1be8b8c0f0f1684b9d4e00ee476d05c15b75e
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-12-17 11:19:15 -0600

    wip

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -6,4 +6,8 @@ describe("Quotes app", () => {
     cy.visit("http://localhost:1234");
   });
   // here go our tests
+  it("sanity test to make sure tests work", () => {
+    // false positive
+    expect(1 + 2).to.equal(3);
+  });
 });
