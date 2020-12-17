8ba2fe5b4acea9d68d4be5a3d9c54ce775198b7b
Rhiannon <11739072+rhiroy@users.noreply.github.com>
2020-12-17 11:23:53 -0600

    wip

--- cypress/integration/quotes.spec.js
+++ cypress/integration/quotes.spec.js
@@ -8,7 +8,11 @@ describe("Quotes app", () => {
   // here go our tests
   it("sanity test to make sure tests work", () => {
     // false positive
+    // 'expect' is an assertion
+    // there can be many assertions per test
+    // inside the 'it' statement (test) many assertions may be
+    // logically grouped together
     expect(1 + 2).to.equal(3);
-    expect(2 + 2).to.equal(5);
+    expect(2 + 2).not.to.equal(5);
   });
 });
